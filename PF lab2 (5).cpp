ae6d334d911bcaa0fa85b45d303e2bd43ac66a1e
ByteSlinger-hue <zoyanattiq13@gmail.com>
2025-12-02 15:54:09 +0500

    Pf labs
    My c programs

---
+++ PF lab2 (5).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout <<"\nEnter Student name:\tZoyan Attiq";
+	cout <<"\nEnter Roll No:\t\t2025-SET-28";
+	cout <<"\nEnter Department:\tSoftware Engineering";
+	cout <<"\nEnter marks in English:\t76";
+	cout <<"\nEnter marks in Math:\t68";
+	cout <<"\nEnter marks in Physics:\t50";
+	return 0;
+}
